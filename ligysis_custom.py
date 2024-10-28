21a02aeb2c03e2243eebece005b321bf29b9def0
Javier Sanchez Utges <2394007@dundee.ac.uk>
2024-10-28 11:39:28 +0000

    Updated colors path

--- ligysis_custom.py
+++ ligysis_custom.py
@@ -123,7 +123,7 @@ interaction_to_color = { # following Arpeggio's colour scheme
     'weak_polar': '#fc7600',
 }
 
-bss_colors = load_pickle("./sample_colors_hex.pkl") # sample colors
+bss_colors = load_pickle("./colors.pkl") # sample colors
 
 headings = ["ID", "RSA", "DS", "MES", "Size", "Cluster", "FS"]
 
